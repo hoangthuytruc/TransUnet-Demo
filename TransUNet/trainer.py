6e3fd373ac16a8e9d60e6ee5144e4ab8168bb847
Truc Hoang <hoangthuytruc@gmail.com>
2025-08-26 00:20:33 +0700

    fix module not found

--- TransUNet/trainer.py
+++ TransUNet/trainer.py
@@ -14,10 +14,10 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from utils import DiceLoss
 from torchvision import transforms
+from dataset_synapse import Synapse_dataset, RandomGenerator
 sys.path.append("/kaggle/working/TransUnet-Demo/TransUNet")
 
 def trainer_synapse(args, model, snapshot_path):
-    from dataset_synapse import Synapse_dataset, RandomGenerator
     logging.basicConfig(filename=snapshot_path + "/log.txt", level=logging.INFO,
                         format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
